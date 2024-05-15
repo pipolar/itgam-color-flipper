9309d8b30832dd4fb4bc04f7d666d7a2e9883954
israel <kevinramirez1j@gmail.com>
2024-05-15 10:39:19 -0600

    Se crearon 5 archivos, 2 javaScript, dos html y uno css en el que por medio el index.html y app.js cambiamos el color del fondo de un contenedor

---
+++ app.js
@@ -0,0 +1,24 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+//Get the button element
+const btn = document.getElementById("btn");
+// Het the color element
+const color = document.querySelector(".color");
+
+// Add event listener to the button
+btn.addEventListener("click", () => {
+    // Obtener un numero aleatorio del 0 al 3
+    const randomNumber = getRandomNumber();
+    //Seleccionando un elemento
+    //Del arreglo colors
+    const colorPicked = colors[randomNumber];
+    // Cambiar el color del body
+    document.body.style.backgroundColor = colorPicked;
+    // Cambiar el color del texto
+    color.textContent = colorPicked;
+});
+
+// Función para obtener un número aleatorio
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
